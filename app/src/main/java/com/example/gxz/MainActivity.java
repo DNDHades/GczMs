644200c0e9b3b25fa360f46e285c129bc95575b0
Nagisa <845538426@qq.com>
2020-06-01 17:35:38 +0800

    更新

--- app/src/main/java/com/example/gxz/MainActivity.java
+++ app/src/main/java/com/example/gxz/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         WeixinUSer uSer2 = new WeixinUSer("2");
         WeixinUSer uSer3 = new WeixinUSer("3");
 
+
         subscriptionSubject.attach(uSer1);
         subscriptionSubject.attach(uSer2);
         subscriptionSubject.attach(uSer3);
